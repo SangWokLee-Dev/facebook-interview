bd0e9ca52a809b88c2df8a1e3fe854e5c655d7d4
Sang Wok Lee <sangwok.lee@bbc.co.uk>
2020-12-04 09:14:17 +0000

    added another example test

--- src/test/java/com/facebook/leetcode/easy/BSTTest.java
+++ src/test/java/com/facebook/leetcode/easy/BSTTest.java
@@ -9,14 +9,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BSTTest {
 
   @Test
-  public void testBST() {
-    TreeNode leftTreeNode = new TreeNode(5, new TreeNode(3), new TreeNode(7));
-    TreeNode rightTreeNode = new TreeNode(15, null, new TreeNode(18));
-    TreeNode treeNode = new TreeNode(10, leftTreeNode, rightTreeNode);
+  public void testBSTExampleOne() {
+    TreeNode treeNode =
+        new TreeNode(
+            10,
+            new TreeNode(5, new TreeNode(3), new TreeNode(7)),
+            new TreeNode(15, null, new TreeNode(18)));
 
     BST bst = new BST();
     bst.dfs(treeNode, 7, 15);
 
     assertEquals(32, bst.rangeSumBST(treeNode, 7, 15));
+  }
+
+  @Test
+  public void testBSTExampleTwo() {
+    TreeNode treeNode =
+        new TreeNode(
+            10,
+            new TreeNode(
+                5, new TreeNode(3, new TreeNode(1), null), new TreeNode(7, new TreeNode(6), null)),
+            new TreeNode(15, new TreeNode(13), new TreeNode(18)));
+    BST bst = new BST();
+    bst.dfs(treeNode, 6, 10);
+
+    assertEquals(23, bst.rangeSumBST(treeNode, 6, 10));
+
   }
 }
